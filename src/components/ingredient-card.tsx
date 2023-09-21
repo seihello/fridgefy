7a45dbcc36d4098eebb2869b778e6b569b57f3fb
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-21 14:01:42 -0700

    feat: remove an ingredient

--- src/components/ingredient-card.tsx
+++ src/components/ingredient-card.tsx
@@ -1,23 +1,40 @@
 import React from 'react'
-import { Flex, Paper, Text, Image } from '@mantine/core'
+import { Flex, Paper, Text, Image, ActionIcon, Group } from '@mantine/core'
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { useContext } from 'react';
+import { FridgeContext } from '@/context/fridge-context';
 
 type Props = {
   name: string;
 }
 
-export default function IngredientCard({name}: Props) {
-  return (
-    <Paper w={300} px='xl' py='sm' shadow='xl' withBorder>
-      <Flex justify='space-between' align='center'>
-        <Text>{name}</Text>
-        <Image
-          src={`https://spoonacular.com/cdn/ingredients_100x100/${name}.jpg`}
-          maw={80}
-          height={40}
+export default function IngredientCard({ name }: Props) {
 
-          fit='contain'
-        />
+  const { myFridgeDispatch } = useContext(FridgeContext);
+
+  return (
+    <Paper w={300} p='sm' shadow='xl' withBorder>
+      <Flex justify='space-between' align='center'>
+        <Flex align='center' columnGap='sm'>
+          <Text>{name}</Text>
+          <Image
+            src={`https://spoonacular.com/cdn/ingredients_100x100/${name}.jpg`}
+            width={60}
+            height={40}
+            fit='contain'
+          />
+        </Flex>
+        <ActionIcon
+          onClick={() => myFridgeDispatch({
+            type: 'remove',
+            payload: name
+          })
+          }
+        >
+          <AiFillCloseCircle size={24} />
+        </ActionIcon>
+
       </Flex>
-    </Paper>
+    </Paper >
   )
 }
