8ba7178a2702ea506a275e17526e21a0ff068fcb
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-09-26 09:36:53 -0700

    feat: get recipes from API

--- src/pages/search-recipes/index.tsx
+++ src/pages/search-recipes/index.tsx
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
 import { Container } from "@mantine/core";
 
 export default function SearchRecipes() {
 
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const response = await fetch("/data/recipes.json");
+        const data = await response.json();
+        setRecipes(data.results);
+      } catch (error) {
+        console.error("Error fetching recipes:", error);
+      }
+    };
+
+    fetchRecipes();
+  }, []);
   return (
     <Container
       p="xl"
