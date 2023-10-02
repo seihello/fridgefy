6fd383cca687b3c62d401ff092d94bc8ea1bf527
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-10-01 23:16:17 -0700

    feat: filter recipes by cuisines

--- src/components/search-recipes/filtered-recipes.tsx
+++ src/components/search-recipes/filtered-recipes.tsx
@@ -7,13 +7,15 @@ import FilteredRecipe from './filtered-recipe';
 
 export default function FilteredRecipes() {
 
-  const { selectedCuisines, selectedIntolerances } = useContext(RecipeContext);
+  const { selectedCuisines, selectedIntolerances, selectedIngredients, isFridgeFilterChecked } = useContext(RecipeContext);
+  const [recipes, setRecipes] = useState<any[]>([]);
   const [filteredRecipes, setFilteredRecipes] = useState<any[]>([]);
 
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
         const result = await axios('/recipes.json');
+        setRecipes(result.data.results);
         setFilteredRecipes(result.data.results);
       } catch (error) {
         console.error('Error fetching the data', error);
@@ -21,7 +23,18 @@ export default function FilteredRecipes() {
     };
 
     fetchRecipes();
-  }, [selectedCuisines, selectedIntolerances]);
+  }, []);
+
+  useEffect(() => {
+    const filterRecipes = async () => {
+      const newFilteredRecipes = recipes.filter((recipe) => {
+        return recipe.cuisines.some((cuisine: string) => selectedCuisines.includes(cuisine));
+      });
+      setFilteredRecipes(newFilteredRecipes);
+    };
+
+    filterRecipes();
+  }, [selectedCuisines, selectedIntolerances, selectedIngredients, isFridgeFilterChecked]);
 
   return (
     <Flex wrap="wrap" rowGap='xl' columnGap='md'>
