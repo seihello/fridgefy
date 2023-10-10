2ba89c555e2cb2fe814fe84d9479f874f2afdafd
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-10-09 21:35:32 -0700

    improvement: redirect to /search-recipes from the root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
 export default function Home() {
+
+  const router = useRouter();
+
+  useEffect(() => {
+      router.push('/search-recipes');
+  }, []);
+
   return (
     <>
-      <p>Hello World</p>
     </>
   )
 }
