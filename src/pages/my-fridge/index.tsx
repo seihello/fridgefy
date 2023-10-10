65ad837efbd7d2b1008d0c265a694c40c22ed1f8
Seisuke Yamada <momoiropuchoman@gmail.com>
2023-10-09 21:03:27 -0700

    improvement: show the note only when the user is not logged in

--- src/pages/my-fridge/index.tsx
+++ src/pages/my-fridge/index.tsx
@@ -1,8 +1,12 @@
+import { useContext } from "react";
 import { Stack, Title, Text, Flex } from "@mantine/core";
 import IngredientSearchBox from "@/components/my-fridge/search-ingredient";
 import IngredientList from "@/components/my-fridge/ingredient-list";
+import { UserContext } from "@/context/user-context";
 
 export default function MyFridge() {
+
+  const { userStatus } = useContext(UserContext);
 
   return (
     <Stack
@@ -10,7 +14,7 @@ export default function MyFridge() {
     >
       <Flex align='flex-end' columnGap='lg'>
         <Title order={2}>Add Ingredients to Your Fridge</Title>
-        <Text >*Please login to save my fridge</Text>
+        {!userStatus && <Text >*Please login to save my fridge</Text>}
       </Flex>
       <IngredientSearchBox />
       <IngredientList />
